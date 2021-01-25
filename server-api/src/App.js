47a5c0fb77eeb006ae7884b62e3b98d17868a692
Juan Carlos Agreda <JuanCarlos@MacBook-Pro.local>
2021-01-25 20:40:08 +0100

    successfully connected to the database

--- server-api/src/App.js
+++ server-api/src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { grocery: [] };
+  };
+
+  componentDidMount() {
+    let url = "http://localhost:8000/grocery"
+    fetch(url)
+      .then(resp => resp.json())
+      .then(data => {
+        let items = data.map((item, i) => {
+          return (
+            <div key={i}>
+              <h3>Product: {item.productName}</h3>
+              <p>Description: {item.productDescription}</p>
+            </div>
+          );
+        })
+        this.setState({ grocery: items });
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.grocery}
+      </div>
+    );
+  }
 }
 
 export default App;
